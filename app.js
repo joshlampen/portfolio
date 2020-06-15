5e0e3fbccc140a85651da3d74daa389cadcaeebe
JoshLampen <joshlampen2@gmail.com>
2020-06-15 00:18:25 +0000

    New screenshots for tweeter and tinyapp; working on 6-project page view

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const app = express();
 app.engine('.html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
+app.use("/assets/scss", sass({
+  src: __dirname + "/assets/scss",
+  dest: __dirname + "/assets/css",
+  debug: true,
+  outputStyle: 'expanded'
+}));
+
 app.use('/assets', express.static('assets'));
 
 app.get('/', (req, res) => {
